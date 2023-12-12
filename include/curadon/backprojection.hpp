c0eed995703e0f347ab18a8d62d29a6d01d52a43
David Frank <frankd@in.tum.de>
2023-12-12 15:00:25 +0100

    more refactoring on backprojection

--- include/curadon/backprojection.hpp
+++ include/curadon/backprojection.hpp
@@ -187,6 +187,25 @@ __global__ void kernel_backprojection(T *volume, std::int64_t stride_x, std::int
     if (idx_x >= vol_shape[0] || idx_y >= vol_shape[1] || start_idx_z >= vol_shape[2])
         return;
 
+    // Each thread has an array of volume voxels, that we first read, then
+    // work on, and then write back once
+    float local_volume[num_voxels_per_thread];
+
+    // First step, load all values into the local_volume array
+#pragma unroll
+    for (int z = 0; z < num_voxels_per_thread; ++z) {
+        const auto idx_z = start_idx_z + z;
+        if (idx_z >= vol_shape[2]) {
+            break;
+        }
+
+        const auto vol_idx = idx_x * stride_x + idx_y * stride_y + idx_z * stride_z;
+
+        local_volume[z] = volume[vol_idx];
+    }
+
+    // Second step, for all projections this kernel performs, do the backprojection
+    // for all voxels this thread is associated with
     for (int i = 0; i < num_projects_per_kernel; ++i) {
         auto idx_proj = cur_projection * num_projects_per_kernel + i;
 
@@ -207,8 +226,6 @@ __global__ void kernel_backprojection(T *volume, std::int64_t stride_x, std::int
                 break;
             }
 
-            const auto vol_idx = idx_x * stride_x + idx_y * stride_y + idx_z * stride_z;
-
             // Compute world coordinates of the working voxel
             // move idx_x delta_x from origin, same for y and z
             auto P = vol_origin + idx_x * delta_x + idx_y * delta_y + idx_z * delta_z;
@@ -226,10 +243,119 @@ __global__ void kernel_backprojection(T *volume, std::int64_t stride_x, std::int
 
             auto sample = tex3D<float>(tex, u, v, i + 0.5f);
 
-            volume[vol_idx] += sample;
+            local_volume[z] += sample;
         }
     }
+
+    // Last step, write local volume back to global one
+#pragma unroll
+    for (int z = 0; z < num_voxels_per_thread; ++z) {
+        const auto idx_z = start_idx_z + z;
+        if (idx_z >= vol_shape[2]) {
+            break;
+        }
+
+        const auto vol_idx = idx_x * stride_x + idx_y * stride_y + idx_z * stride_z;
+
+        volume[vol_idx] = local_volume[z];
+    }
 }
+
+namespace detail {
+cudaArray_t allocate_cuarray(std::size_t width, std::size_t height, std::size_t depth) {
+    const cudaExtent extent_alloc = make_cudaExtent(width, height, depth);
+    cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc<float>();
+    cudaArray_t array;
+    cudaMalloc3DArray(&array, &channelDesc, extent_alloc);
+    gpuErrchk(cudaPeekAtLastError());
+
+    return array;
+}
+
+template <class T>
+void copy_projections_to_array(T *sinogram, std::size_t offset, cudaArray_t array_cu,
+                               std::size_t width, std::size_t height, std::size_t num_projections) {
+    // Copy to cuda array
+    cudaMemcpy3DParms copyParams = {0};
+
+    auto ptr = sinogram + offset;
+    copyParams.srcPtr = make_cudaPitchedPtr((void *)ptr, width * sizeof(float), width, height);
+
+    const cudaExtent extent = make_cudaExtent(width, height, num_projections);
+    gpuErrchk(cudaPeekAtLastError());
+    copyParams.dstArray = array_cu;
+    copyParams.extent = extent;
+    copyParams.kind = cudaMemcpyDefault;
+    cudaMemcpy3DAsync(&copyParams, 0); // TODO: use stream pool
+    cudaStreamSynchronize(0);
+    gpuErrchk(cudaPeekAtLastError());
+}
+
+void copy_to_texture(cudaTextureObject_t *tex, cudaArray_t array_cu) {
+    cudaResourceDesc texRes;
+    memset(&texRes, 0, sizeof(cudaResourceDesc));
+    texRes.resType = cudaResourceTypeArray;
+    texRes.res.array.array = array_cu;
+    cudaTextureDesc texDescr;
+    memset(&texDescr, 0, sizeof(cudaTextureDesc));
+    texDescr.normalizedCoords = false;
+    texDescr.filterMode = cudaFilterModeLinear;
+    texDescr.addressMode[0] = cudaAddressModeBorder;
+    texDescr.addressMode[1] = cudaAddressModeBorder;
+    texDescr.addressMode[2] = cudaAddressModeBorder;
+    texDescr.readMode = cudaReadModeElementType;
+
+    cudaCreateTextureObject(tex, &texRes, &texDescr, NULL);
+    gpuErrchk(cudaPeekAtLastError());
+}
+
+void setup_constants(std::size_t start_proj, std::size_t num_projections, Vec<float, 3> vol_size,
+                     Vec<float, 3> vol_spacing, Vec<float, 3> vol_offset,
+                     const std::vector<float> &angles) {
+    std::vector<curad::Vec<float, 3>> vol_origins;
+    std::vector<curad::Vec<float, 3>> delta_xs;
+    std::vector<curad::Vec<float, 3>> delta_ys;
+    std::vector<curad::Vec<float, 3>> delta_zs;
+    for (int j = start_proj; j < start_proj + num_projections; ++j) {
+        float angle = angles[j] * M_PI / 180.f;
+
+        curad::Vec<float, 3> init_vol_origin = -vol_size / 2.f + vol_spacing / 2.f + vol_offset;
+        auto vol_origin = curad::detail::rotate_yzy(init_vol_origin, angle, 0.f, 0.f);
+        vol_origins.push_back(vol_origin);
+
+        curad::Vec<float, 3> init_delta;
+        init_delta = init_vol_origin;
+        init_delta[0] += vol_spacing[0];
+        init_delta = curad::detail::rotate_yzy(init_delta, angle, 0.f, 0.f);
+        delta_xs.push_back(init_delta - vol_origin);
+
+        init_delta = init_vol_origin;
+        init_delta[1] += vol_spacing[1];
+        init_delta = curad::detail::rotate_yzy(init_delta, angle, 0.f, 0.f);
+        delta_ys.push_back(init_delta - vol_origin);
+
+        init_delta = init_vol_origin;
+        init_delta[2] += vol_spacing[2];
+        init_delta = curad::detail::rotate_yzy(init_delta, angle, 0.f, 0.f);
+        delta_zs.push_back(init_delta - vol_origin);
+    }
+
+    cudaMemcpyToSymbol(curad::dev_vol_origin, vol_origins.data(),
+                       sizeof(curad::Vec<float, 3>) * curad::num_projects_per_kernel, 0,
+                       cudaMemcpyDefault);
+    cudaMemcpyToSymbol(curad::dev_delta_x, delta_xs.data(),
+                       sizeof(curad::Vec<float, 3>) * curad::num_projects_per_kernel, 0,
+                       cudaMemcpyDefault);
+    cudaMemcpyToSymbol(curad::dev_delta_y, delta_ys.data(),
+                       sizeof(curad::Vec<float, 3>) * curad::num_projects_per_kernel, 0,
+                       cudaMemcpyDefault);
+    cudaMemcpyToSymbol(curad::dev_delta_z, delta_zs.data(),
+                       sizeof(curad::Vec<float, 3>) * curad::num_projects_per_kernel, 0,
+                       cudaMemcpyDefault);
+    gpuErrchk(cudaPeekAtLastError());
+    gpuErrchk(cudaDeviceSynchronize());
+}
+} // namespace detail
 
 template <class T, class U>
 void backproject_3d(T *volume, std::int64_t stride_x, std::int64_t stride_y, std::int64_t stride_z,
@@ -241,12 +367,7 @@ void backproject_3d(T *volume, std::int64_t stride_x, std::int64_t stride_y, std
     const auto nangles = angles.size();
 
     // allocate cuda array for sinogram
-    const cudaExtent extent_alloc =
-        make_cudaExtent(sino_width, sino_height, curad::num_projects_per_kernel);
-    cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc<float>();
-    cudaArray_t array_cu;
-    cudaMalloc3DArray(&array_cu, &channelDesc, extent_alloc);
-    gpuErrchk(cudaPeekAtLastError());
+    auto array_cu = detail::allocate_cuarray(sino_width, sino_height, num_projects_per_kernel);
 
     cudaTextureObject_t tex;
 
@@ -255,88 +376,24 @@ void backproject_3d(T *volume, std::int64_t stride_x, std::int64_t stride_y, std
     for (int i = 0; i < num_kernel_calls; ++i) {
         auto proj_idx = i * curad::num_projects_per_kernel;
 
-        // Copy to cuda array
-        cudaMemcpy3DParms copyParams = {0};
-        gpuErrchk(cudaPeekAtLastError());
-
-        auto ptr = sinogram + proj_idx * sino_width * sino_height;
-        copyParams.srcPtr =
-            make_cudaPitchedPtr((void *)ptr, sino_width * sizeof(float), sino_width, sino_height);
-
         auto projections_left = nangles - (i * curad::num_projects_per_kernel);
-        const cudaExtent extent =
-            make_cudaExtent(sino_width, sino_height,
-                            std::min<int>(curad::num_projects_per_kernel, projections_left));
-        // std::cout << "sino size: " << sino.size() << "\n";
-        // std::cout << "offset: " << proj_idx * width * height << "\n";
-        // std::cout << "extent: " << extent.width << " " << extent.height << " " << extent.depth
-        //           << "\n";
-        gpuErrchk(cudaPeekAtLastError());
-        copyParams.dstArray = array_cu;
-        copyParams.extent = extent;
-        copyParams.kind = cudaMemcpyDefault;
-        cudaMemcpy3DAsync(&copyParams, 0); // TODO: use stream pool
-        cudaStreamSynchronize(0);
-        gpuErrchk(cudaPeekAtLastError());
 
-        cudaResourceDesc texRes;
-        memset(&texRes, 0, sizeof(cudaResourceDesc));
-        texRes.resType = cudaResourceTypeArray;
-        texRes.res.array.array = array_cu;
-        cudaTextureDesc texDescr;
-        memset(&texDescr, 0, sizeof(cudaTextureDesc));
-        texDescr.normalizedCoords = false;
-        texDescr.filterMode = cudaFilterModeLinear;
-        texDescr.addressMode[0] = cudaAddressModeBorder;
-        texDescr.addressMode[1] = cudaAddressModeBorder;
-        texDescr.addressMode[2] = cudaAddressModeBorder;
-        texDescr.readMode = cudaReadModeElementType;
+        // On how many projections do we work on this call? either num_projects_per_kernel, or
+        // what ever is left
+        const auto num_projections =
+            std::min<int>(curad::num_projects_per_kernel, projections_left);
 
-        cudaCreateTextureObject(&tex, &texRes, &texDescr, NULL);
-        gpuErrchk(cudaPeekAtLastError());
+        // Copy projection data necessary for the next kernel to cuda array
+        detail::copy_projections_to_array(sinogram, proj_idx * sino_width * sino_height, array_cu,
+                                          sino_width, sino_height, num_projections);
 
-        std::vector<curad::Vec<float, 3>> vol_origins;
-        std::vector<curad::Vec<float, 3>> delta_xs;
-        std::vector<curad::Vec<float, 3>> delta_ys;
-        std::vector<curad::Vec<float, 3>> delta_zs;
-        for (int j = 0; j < curad::num_projects_per_kernel; ++j) {
-            float angle = angles[proj_idx + j] * M_PI / 180.f;
+        // Bind cuda array containing the sinogram to the texture
+        detail::copy_to_texture(&tex, array_cu);
 
-            curad::Vec<float, 3> init_vol_origin = -vol_size / 2.f + vol_spacing / 2.f + vol_offset;
-            auto vol_origin = curad::detail::rotate_yzy(init_vol_origin, angle, 0.f, 0.f);
-            vol_origins.push_back(vol_origin);
-
-            curad::Vec<float, 3> init_delta;
-            init_delta = init_vol_origin;
-            init_delta[0] += vol_spacing[0];
-            init_delta = curad::detail::rotate_yzy(init_delta, angle, 0.f, 0.f);
-            delta_xs.push_back(init_delta - vol_origin);
-
-            init_delta = init_vol_origin;
-            init_delta[1] += vol_spacing[1];
-            init_delta = curad::detail::rotate_yzy(init_delta, angle, 0.f, 0.f);
-            delta_ys.push_back(init_delta - vol_origin);
-
-            init_delta = init_vol_origin;
-            init_delta[2] += vol_spacing[2];
-            init_delta = curad::detail::rotate_yzy(init_delta, angle, 0.f, 0.f);
-            delta_zs.push_back(init_delta - vol_origin);
-        }
-
-        cudaMemcpyToSymbol(curad::dev_vol_origin, vol_origins.data(),
-                           sizeof(curad::Vec<float, 3>) * curad::num_projects_per_kernel, 0,
-                           cudaMemcpyDefault);
-        cudaMemcpyToSymbol(curad::dev_delta_x, delta_xs.data(),
-                           sizeof(curad::Vec<float, 3>) * curad::num_projects_per_kernel, 0,
-                           cudaMemcpyDefault);
-        cudaMemcpyToSymbol(curad::dev_delta_y, delta_ys.data(),
-                           sizeof(curad::Vec<float, 3>) * curad::num_projects_per_kernel, 0,
-                           cudaMemcpyDefault);
-        cudaMemcpyToSymbol(curad::dev_delta_z, delta_zs.data(),
-                           sizeof(curad::Vec<float, 3>) * curad::num_projects_per_kernel, 0,
-                           cudaMemcpyDefault);
-        gpuErrchk(cudaPeekAtLastError());
-        gpuErrchk(cudaDeviceSynchronize());
+        // kernel uses variables stored in __constant__ memory, e.g. volume origin, volume
+        // deltas Compute them here and upload them
+        detail::setup_constants(proj_idx, num_projections, vol_size, vol_spacing, vol_offset,
+                                angles);
 
         int divx = 16;
         int divy = 32;
