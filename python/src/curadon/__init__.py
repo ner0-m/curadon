66e72b41935b4457b40d54ede141fd95021619ca
David Frank <frankd@in.tum.de>
2024-01-30 16:29:19 +0100

    expose stream and events in python

--- python/src/curadon/__init__.py
+++ python/src/curadon/__init__.py
@@ -8,3 +8,5 @@ from .geometry import FanGeometry, ConeGeometry
 from .forward import forward
 from .backward import backward
 from .diffable import TorchForward, TorchBackprojection
+
+from .curadon_ext import event, event_view, event_pool, stream, stream_view, stream_pool, get_next_stream, get_stream, get_next_event, get_event
